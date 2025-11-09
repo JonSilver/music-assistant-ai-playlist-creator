b0803501bf2136f847ed5b06563e10020b706c8e
Jon Silver <js@jfdi.info>
2025-11-09 22:57:49 +0000

    fix: frontend localhost fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.FRONTEND_PORT ?? '5555', 10),
+    host: true,
+    port: parseInt(process.env.FRONTEND_PORT ?? '5555', 10)
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@shared': path.resolve(__dirname, '../shared'),
-    },
-  },
+      '@shared': path.resolve(__dirname, '../shared')
+    }
+  }
 })
